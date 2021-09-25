91a4f00a9cd6fb5a49f9bbbfe9f755fd29a6e762
Maxim Parsin <maxim.parsin@gmail.com>
2021-09-25 07:03:30 -0400

    chore: Add Angular Material to the workspace

--- apps/eqms/src/app/app.module.ts
+++ apps/eqms/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WebShellModule } from '@qad-nx/web/shell/feature';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    WebShellModule
+    WebShellModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
